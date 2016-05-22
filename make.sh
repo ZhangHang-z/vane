f968f3f439b62b004867a3089cf5adf6baa5c76c
ZhangHang <stevezhang2014@gmail.com>
2016-05-22 17:01:24 +0800

    initialize make.sh file for generate executable script

---
+++ make.sh
@@ -0,0 +1,14 @@
+#!/usr/env sh
+
+echo "installing vane package manager"
+
+if type go 2> /dev/null; then 
+	echo "error: make sure you have golang installed"
+	exit 1
+else
+	cd "./bin"
+	if go install vane-cli.go 2> /dev/null; then 
+		echo "error: install vane-cli failed"
+	else
+		ln -s /usr/bin/vane $GOPATH/bin/vane-cli
+fi
